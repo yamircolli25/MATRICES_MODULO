ce31eba4996e06db31f5b54fba57a386b4f43f50
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-09-11 15:48:34 -0600

    .

--- src/componentes/testing/App.test.tsx
+++ src/componentes/testing/App.test.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Counter from '../src/App';
+import Counter from "../../App";
 import '@testing-library/jest-dom';
 
 describe('Counter', () => {
     test('renders initial count', () => {
         render(<Counter />);
-        const countElement = screen.getByText('Contador:0');
+        const countElement = screen.getByText('Contador: 0');
         expect(countElement).toBeInTheDocument();
     });
 
@@ -17,7 +17,7 @@ describe('Counter', () => {
             name: /Incrementar/i,
         });
         await userEvent.click(incrementButton);
-        const updatedCountElement = screen.getByText('Contador:1');
+        const updatedCountElement = screen.getByText('Contador: 1');
         expect(updatedCountElement).toBeInTheDocument();
     });
 
@@ -27,7 +27,7 @@ describe('Counter', () => {
             name: /Decrementar/i,
         });
         await userEvent.click(decrementButton);
-        const updatedCountElement = screen.getByText('Contador:-1');
+        const updatedCountElement = screen.getByText('Contador: -1');
         expect(updatedCountElement).toBeInTheDocument();
     });
 
