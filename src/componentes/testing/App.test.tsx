174797d9fc72736dde7800451778d6c5aeef0122
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-09-11 15:18:12 -0600

    .

---
+++ src/componentes/testing/App.test.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Counter from '../src/App';
+import '@testing-library/jest-dom';
+
+describe('Counter', () => {
+    test('renders initial count', () => {
+        render(<Counter />);
+        const countElement = screen.getByText('Contador:0');
+        expect(countElement).toBeInTheDocument();
+    });
+
+    test('increments count when button is clicked', async () => {
+        render(<Counter />);
+        const incrementButton = screen.getByRole('button', {
+            name: /Incrementar/i,
+        });
+        await userEvent.click(incrementButton);
+        const updatedCountElement = screen.getByText('Contador:1');
+        expect(updatedCountElement).toBeInTheDocument();
+    });
+
+    test('decrements count when button is clicked', async () => {
+        render(<Counter />);
+        const decrementButton = screen.getByRole('button', {
+            name: /Decrementar/i,
+        });
+        await userEvent.click(decrementButton);
+        const updatedCountElement = screen.getByText('Contador:-1');
+        expect(updatedCountElement).toBeInTheDocument();
+    });
+
+    test('matches snapshot', () => {
+        const { asFragment } = render(<Counter />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
