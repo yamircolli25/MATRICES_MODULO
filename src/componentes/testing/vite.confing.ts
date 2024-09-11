a4254ba59d71d16ba120051ad12b5fb9819a9d12
yamircolli25 <ycolli.24431@umsa.edu.mx>
2024-09-11 16:14:06 -0600

    .

--- src/componentes/testing/vite.confing.ts
+++ src/componentes/testing/vite.confing.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.ts',
-    },
+    test:{
+        globals:true,
+        enviroment:'jsdom',
+        setupFiles:'./src/setupTests.ts'
+    }
 });
